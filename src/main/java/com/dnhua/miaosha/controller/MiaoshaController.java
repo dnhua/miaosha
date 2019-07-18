169bda5d95d144d1a96c35a02c846080b3f3e254
dnhua <huazailixtu@outlook.com>
2019-07-18 20:53:40 +0800

    完成验证码的生成

--- src/main/java/com/dnhua/miaosha/controller/MiaoshaController.java
+++ src/main/java/com/dnhua/miaosha/controller/MiaoshaController.java
@@ -21,6 +21,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 
@@ -180,5 +184,25 @@ public class MiaoshaController implements InitializingBean {
 		}
 		long result  =miaoshaService.getMiaoshaResult(user.getId(), goodsId);
 		return Result.success(result);
+	}
+
+	@RequestMapping(value="/verifyCode", method=RequestMethod.GET)
+	@ResponseBody
+	public Result<String> getMiaoshaVerifyCod(HttpServletResponse response, MiaoshaUser user,
+											  @RequestParam("goodsId")long goodsId) {
+		if(user == null) {
+			return Result.error(CodeMsg.SESSION_ERROR);
+		}
+		try {
+			BufferedImage image  = miaoshaService.createVerifyCode(user, goodsId);
+			OutputStream out = response.getOutputStream();
+			ImageIO.write(image, "JPEG", out);
+			out.flush();
+			out.close();
+			return null;
+		}catch(Exception e) {
+			e.printStackTrace();
+			return Result.error(CodeMsg.MIAOSHA_FAIL);
+		}
 	}
 }
